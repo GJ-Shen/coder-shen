a8bb46b3f169c55b9af96739c849c1eaa2e6ab74
spicy lee <794566756@qq.com>
2025-01-09 23:13:29 +0800

    update

---
+++ leetcode/轮转数组.ts
@@ -0,0 +1,16 @@
+function rotate(nums: number[], k: number): void {
+    k = k % nums.length;
+    reverse(nums,0,nums.length-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,nums.length-1)
+};
+
+const reverse = (nums,start,end) => {
+    while(start<end){
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
