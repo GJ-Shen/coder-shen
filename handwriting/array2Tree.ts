5661b2af81e7a6fe6c35070152d22fbef2012e3d
spicy lee <794566756@qq.com>
2025-01-15 21:51:09 +0800

    chore

---
+++ handwriting/array2Tree.ts
@@ -0,0 +1,24 @@
+function arrayToTree(data: { id: string; parentId: string | null; [key: string]: any } []) {
+    const map = {};
+    const result = [];
+  
+    // 1. 遍历数据，将每个节点以 id 为键存储到 map 中
+    data.forEach(item => {
+      map[item.id] = { ...item, children: [] };
+    });
+  
+    // 2. 遍历数据，将每个节点挂载到其父节点的 children 数组中
+    data.forEach(item => {
+      if (item.parentId === null) {
+        // 根节点直接添加到结果中
+        result.push(map[item.id]);
+      } else {
+        // 非根节点将自己添加到父节点的 children 数组
+        if (map[item.parentId]) {
+          map[item.parentId].children.push(map[item.id]);
+        }
+      }
+    });
+  
+    return result;
+  }
