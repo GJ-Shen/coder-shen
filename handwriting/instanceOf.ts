ce1bc78df2622474c1a3b882d99c0a5166a897c3
spicy lee <794566756@qq.com>
2025-02-06 14:54:26 +0800

    chore: update

---
+++ handwriting/instanceOf.ts
@@ -0,0 +1,15 @@
+function myInstanceOf(obj, constructor) {
+    // 获取对象的原型
+    let prototype = Object.getPrototypeOf(obj);
+  
+    // 一直沿着原型链查找，直到找到构造函数的 prototype 或者 null
+    while (prototype !== null) {
+      if (prototype === constructor.prototype) {
+        return true; // 找到构造函数的 prototype，返回 true
+      }
+      prototype = Object.getPrototypeOf(prototype); // 继续查找父级原型
+    }
+  
+    return false; // 没找到，返回 false
+  }
+  
