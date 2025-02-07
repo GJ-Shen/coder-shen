ff45ae97a6edf5178c150320415558af88990acc
spicy lee <794566756@qq.com>
2025-02-08 00:28:12 +0800

    chore: update

---
+++ handwriting/objectMethod.ts
@@ -0,0 +1,5 @@
+function myNew (constructor, ...args) {
+    const obj = Object.create(constructor.prototype)
+    const result = constructor.apply(obj, args)
+    return typeof result === 'object' && result !== null ? result : obj
+}
