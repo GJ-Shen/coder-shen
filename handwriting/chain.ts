cfc1958a3d148911be02e76445cce871cd915b72
spicy lee <794566756@qq.com>
2025-02-08 20:30:48 +0800

    chore: update

---
+++ handwriting/chain.ts
@@ -0,0 +1,58 @@
+class Calculator {
+    value: number
+    constructor (value) {
+        this.value = value
+    }
+
+    add(num) {
+        this.value += num
+        return this
+    }
+
+    decrease(num) {
+        this.value -= num
+        return this
+    }
+
+    times(num) {
+        this.value *= num
+        return this
+    }
+
+    divide(num) {
+        this.value /= num
+        return this
+    }
+
+    getValue() {
+        return this.value
+    }
+}
+
+function calculator (value: number) {
+    return {
+        add (num) {
+            value += num
+            return this
+        },
+    
+        decrease (num) {
+           value -= num
+            return this
+        },
+    
+        times (num) {
+           value *= num
+            return this
+        },
+    
+        divide (num) {
+            value /= num
+            return this
+        },
+    
+        getValue () {
+            return value
+        }
+    }
+}
