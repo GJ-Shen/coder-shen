5275c4d64b0bf660817413e1a1fc893c335cabb7
spicy lee <794566756@qq.com>
2025-02-08 21:00:46 +0800

    chore: update

--- handwriting/chain.ts
+++ handwriting/chain.ts
@@ -1,59 +1,59 @@
 // 关键字：各方法返回this
 class Calculator {
-    value: number
-    constructor (value) {
-        this.value = value
-    }
+  value: number;
+  constructor(value) {
+    this.value = value;
+  }
 
-    add(num) {
-        this.value += num
-        return this
-    }
+  add(num) {
+    this.value += num;
+    return this;
+  }
 
-    decrease(num) {
-        this.value -= num
-        return this
-    }
+  decrease(num) {
+    this.value -= num;
+    return this;
+  }
 
-    times(num) {
-        this.value *= num
-        return this
-    }
+  times(num) {
+    this.value *= num;
+    return this;
+  }
 
-    divide(num) {
-        this.value /= num
-        return this
-    }
+  divide(num) {
+    this.value /= num;
+    return this;
+  }
 
-    getValue() {
-        return this.value
-    }
+  getValue() {
+    return this.value;
+  }
 }
 
-function calculator (value: number) {
-    return {
-        add (num) {
-            value += num
-            return this
-        },
-    
-        decrease (num) {
-            value -= num
-            return this
-        },
-    
-        times (num) {
-            value *= num
-            return this
-        },
-    
-        divide (num) {
-            value /= num
-            return this
-        },
-    
-        getValue () {
-            return value
-        }
-    }
+function calculator(value: number) {
+  return {
+    add(num) {
+      value += num;
+      return this;
+    },
+
+    decrease(num) {
+      value -= num;
+      return this;
+    },
+
+    times(num) {
+      value *= num;
+      return this;
+    },
+
+    divide(num) {
+      value /= num;
+      return this;
+    },
+
+    getValue() {
+      return value;
+    },
+  };
 }
